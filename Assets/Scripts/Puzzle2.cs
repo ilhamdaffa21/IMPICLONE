99bf3fc5b27fd5506aae90f2571c648b65b4cc9b
Skicom123 <fajarrahmanarajab@gmail.com>
2022-01-01 23:47:40 +0700

    Update Piano Puzzle 2

---
+++ Assets/Scripts/Puzzle2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle2 : MonoBehaviour {
+
+
+	[SerializeField]
+	GameObject piano, closedSafe, openedSafe;
+
+	public static bool isSafeOpened = false;
+
+	// Use this for initialization
+	void Start () {
+		piano.SetActive (false);
+		closedSafe.SetActive (true);
+		openedSafe.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (isSafeOpened) {
+			piano.SetActive (false);
+			closedSafe.SetActive (false);
+			openedSafe.SetActive (true);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.name.Equals ("Safe") && !isSafeOpened) {
+			piano.SetActive (true);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col)
+	{
+		if (col.gameObject.name.Equals ("Safe")) {
+			piano.SetActive (false);
+		}
+	}
+}
