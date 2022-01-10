712137fa0258d7a4a15149f4096b89b672f10678
Skicom123 <fajarrahmanarajab@gmail.com>
2022-01-10 23:22:35 +0700

    Update Piano Sound

--- Assets/Scripts/Puzzles/2/Piano.cs
+++ Assets/Scripts/Puzzles/2/Piano.cs
@@ -3,52 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Piano : MonoBehaviour {
+public class Piano : MonoBehaviour
+{
 
-	[SerializeField]
-	Text codeText;
-	string[] codeTextValue = { "6", "4", "6", "4#", "6", "5", "2#", "5", "2#", "5", "4#", "4", "3", "2#", "2", "2#", "3", "5#", "5", "2#", "3" };
-	int mark = -1;
-	public AudioSource wrongSound;
+    [SerializeField]
+    Text codeText;
+    string[] codeTextValue = { "6", "4", "6", "4#", "6", "5", "2#", "5", "2#", "5", "4#", "4", "3", "2#", "2", "2#", "3", "5#", "5", "2#", "3" };
+    int mark = -1;
+    public AudioSource wrongSound;
 
 
-	// Update is called once per frame
-	private void Start()
-	{
-	}
-	void Update() {
-		//codeText.text = codeTextValue;
+    // Update is called once per frame
+    private void Start()
+    {
+        wrongSound = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        //codeText.text = codeTextValue;
 
-		//if (codeTextValue == "ABCDEFGHI") {
-		//	wrongSound.Play();
-		//	Puzzle2.isSafeOpened = true;
-		//}
+        //if (codeTextValue == "ABCDEFGHI") {
+        //	wrongSound.Play();
+        //	Puzzle2.isSafeOpened = true;
+        //}
 
-		//if (codeTextValue.Length >= 9)
-		//	codeTextValue = "";
-		//
-		}
+        //if (codeTextValue.Length >= 9)
+        //	codeTextValue = "";
+        //
+    }
 
-	public void AddDigit(string digit)
-	{
-		if (digit != "")
-		{
-			print("input");
-			mark += 1;
-			print(codeTextValue[mark]);
-			//print(mark);
-			//print("ini digit " + digit);
-			if (digit == codeTextValue[mark])
-			{
-				print("benar");
-			}
-			else
-			{
-				print("salah");
-				mark = -1;
-			}
-		}
-	}
+    public void AddDigit(string digit)
+    {
+        if (digit != "")
+        {
+            print("input");
+            mark += 1;
+            print(codeTextValue[mark]);
+            //print(mark);
+            //print("ini digit " + digit);
+            if (digit == codeTextValue[mark])
+            {
+                print("benar");
+            }
+            else
+            {
+                print("salah");
+                mark = -1;
+                wrongSound.Play();
+            }
+        }
+    }
 }
 
 
