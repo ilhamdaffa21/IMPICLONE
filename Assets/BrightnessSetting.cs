b5eefa3fe59f306c4df8104d079be9082fa39a10
StevenJulian1528 <95745789+StevenJulian1528@users.noreply.github.com>
2022-01-13 21:21:53 +0700

    adding canvas settings, fixing grab textbox, adding brightness settings

---
+++ Assets/BrightnessSetting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BrightnessSetting : MonoBehaviour
+{
+    Slider brightslider;
+    public Image brightfade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        brightslider = GetComponent<Slider>();
+        brightslider.value = brightfade.color.a;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        brightfade.color = new Color(brightfade.color.r, brightfade.color.g, brightfade.color.b, brightslider.value);
+    }
+}
--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
+    public GameObject settingsCanvas, pauseCanvas;
 
     public void Pause()
     {
@@ -18,10 +19,27 @@ public class PauseMenu : MonoBehaviour
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
     }
+    public void ResumeSettings()
+    {
+        settingsCanvas.SetActive(false);
+        Time.timeScale = 1f;
+    }
 
     public void Home(int sceneID)
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(sceneID);
+    }
+    public void Settings()
+    {
+        settingsCanvas.SetActive(true);
+        pauseCanvas.SetActive(false);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        //pauseCanvas.SetActive(false);
+        Time.timeScale = 1;
     }
 }
